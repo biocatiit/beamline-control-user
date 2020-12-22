e21a088cfe15524be5a8651f0da01c8fc5c30588
jbhopkins <hopkins.jesse@gmail.com>
2020-12-22 15:27:43 -0600

    Added try/except to server and client to help prevent complete failure if an error happens.

--- biocon/server.py
+++ biocon/server.py
@@ -134,71 +134,75 @@ class ControlServer(threading.Thread):
         """
         while True:
             try:
-                if self.socket.poll(10) > 0:
-                    logger.debug("Getting new command")
-                    command = self.socket.recv_json()
-                else:
-                    command = None
-            except Exception:
-                command = None
-
-            # if self._abort_event.is_set():
-            #     logger.debug("Abort event detected")
-            #     self._abort()
-            #     command = None
-
-            if self._stop_event.is_set():
-                logger.debug("Stop event detected")
-                break
-
-            if command is not None:
-                device = command['device']
-                device_cmd = command['command']
-                get_response = command['response']
-                logger.debug("For device %s, processing cmd '%s' with args: %s and kwargs: %s ", device, device_cmd[0], ', '.join(['{}'.format(a) for a in device_cmd[1]]), ', '.join(['{}:{}'.format(kw, item) for kw, item in device_cmd[2].items()]))
                 try:
-
-                    if device == 'server':
-                        if device_cmd[0] == 'ping':
-                            answer = 'ping received'
-                        else:
-                            answer = ''
+                    if self.socket.poll(10) > 0:
+                        logger.debug("Getting new command")
+                        command = self.socket.recv_json()
                     else:
-                        device_q = self._device_control[device]['queue']
-                        device_q.append(device_cmd)
-
-                        if get_response:
-                            answer_q = self._device_control[device]['answer_q']
-
-                            start_time = time.time()
-                            while len(answer_q) == 0 and time.time()-start_time < 5:
-                                time.sleep(0.01)
-
-                            if len(answer_q) == 0:
-                                answer = ''
-                            else:
-                                answer = answer_q.popleft()
-                        else:
-                            answer = 'cmd sent'
-
-                    if answer == '':
-                        logger.exception('No response received from device')
-                    else:
-                        logger.debug('Sending command response: %s', answer)
-                        self.socket.send_json(answer)
-
+                        command = None
                 except Exception:
+                    command = None
+
+                # if self._abort_event.is_set():
+                #     logger.debug("Abort event detected")
+                #     self._abort()
+                #     command = None
+
+                if self._stop_event.is_set():
+                    logger.debug("Stop event detected")
+                    break
+
+                if command is not None:
                     device = command['device']
                     device_cmd = command['command']
-                    msg = ("Device %s failed to run command '%s' "
-                        "with args: %s and kwargs: %s. Exception follows:" %(device, device_cmd[0],
-                        ', '.join(['{}'.format(a) for a in device_cmd[1]]),
-                        ', '.join(['{}:{}'.format(kw, item) for kw, item in device_cmd[2].items()])))
-                    logger.exception(msg)
-                    logger.exception(traceback.print_exc())
+                    get_response = command['response']
+                    logger.debug("For device %s, processing cmd '%s' with args: %s and kwargs: %s ", device, device_cmd[0], ', '.join(['{}'.format(a) for a in device_cmd[1]]), ', '.join(['{}:{}'.format(kw, item) for kw, item in device_cmd[2].items()]))
+                    try:
 
-            else:
-                time.sleep(0.01)
+                        if device == 'server':
+                            if device_cmd[0] == 'ping':
+                                answer = 'ping received'
+                            else:
+                                answer = ''
+                        else:
+                            device_q = self._device_control[device]['queue']
+                            device_q.append(device_cmd)
+
+                            if get_response:
+                                answer_q = self._device_control[device]['answer_q']
+
+                                start_time = time.time()
+                                while len(answer_q) == 0 and time.time()-start_time < 5:
+                                    time.sleep(0.01)
+
+                                if len(answer_q) == 0:
+                                    answer = ''
+                                else:
+                                    answer = answer_q.popleft()
+                            else:
+                                answer = 'cmd sent'
+
+                        if answer == '':
+                            logger.exception('No response received from device')
+                        else:
+                            logger.debug('Sending command response: %s', answer)
+                            self.socket.send_json(answer)
+
+                    except Exception:
+                        device = command['device']
+                        device_cmd = command['command']
+                        msg = ("Device %s failed to run command '%s' "
+                            "with args: %s and kwargs: %s. Exception follows:" %(device, device_cmd[0],
+                            ', '.join(['{}'.format(a) for a in device_cmd[1]]),
+                            ', '.join(['{}:{}'.format(kw, item) for kw, item in device_cmd[2].items()])))
+                        logger.exception(msg)
+                        logger.exception(traceback.print_exc())
+
+                else:
+                    time.sleep(0.01)
+
+            except Exception:
+                logger.error('Error in server thread:\n{}'.format(traceback.format_exc()))
 
         if self._stop_event.is_set():
             self._stop_event.clear()
