28b4f2bda73df6bcce387570be4d2e0ada363d8a
jbhopkins <hopkins.jesse@gmail.com>
2023-01-27 09:58:41 -0600

    Fixed a bug in continuous exposure times.

--- biocon/biocon.py
+++ biocon/biocon.py
@@ -657,13 +657,13 @@ if __name__ == '__main__':
 
     components = OrderedDict([
         ('exposure', expcon.ExpPanel),
-        ('coflow', coflowcon.CoflowPanel),
+        # ('coflow', coflowcon.CoflowPanel),
         # ('trsaxs_scan', trcon.TRScanPanel),
         # ('trsaxs_flow', trcon.TRFlowPanel),
         # ('scan',    scancon.ScanPanel),
-        ('metadata', metadata.ParamPanel),
+        # ('metadata', metadata.ParamPanel),
         # ('pipeline', pipeline_ctrl.PipelineControl),
-        ('uv', spectrometercon.UVPanel),
+        # ('uv', spectrometercon.UVPanel),
         ])
 
     settings = {
