8b11221431b67f31ef2a0ad3981d68d14c0e2207
jbhopkins <hopkins.jesse@gmail.com>
2023-03-06 16:35:16 -0600

    Changes to active devices in server.py

--- biocon/server.py
+++ biocon/server.py
@@ -393,25 +393,13 @@ if __name__ == '__main__':
         #     {'name': 'sheath', 'args': ['VICI M50', 'COM3'],
         #         'kwargs': {'flow_cal': '627.72', 'backlash_cal': '9.814'},
         #         'ctrl_args': {'flow_rate': 1}},
-        #     {'name': 'outlet', 'args': ['OB1 Pump', 'COM8'],
+        #     {'name': 'outlet', 'args': ['OB1 Pump', 'COM10'],
         #         'kwargs': {'ob1_device_name': 'Outlet OB1', 'channel': 1,
-        #         'min_pressure': -1000, 'max_pressure': 1000, 'P': 5, 'I': 0.00015,
+        #         'min_pressure': -900, 'max_pressure': 1000, 'P': -2, 'I': -0.15,
         #         'D': 0, 'bfs_instr_ID': None, 'comm_lock': ob1_comm_lock,
         #         'calib_path': './resources/ob1_calib.txt'},
         #         'ctrl_args': {}}
         #     ]
-
-        setup_pumps = [
-            {'name': 'sheath', 'args': ['VICI M50', 'COM3'],
-                'kwargs': {'flow_cal': '627.72', 'backlash_cal': '9.814'},
-                'ctrl_args': {'flow_rate': 1}},
-            {'name': 'outlet', 'args': ['OB1 Pump', 'COM3'],
-                'kwargs': {'ob1_device_name': 'Outlet OB1', 'channel': 1,
-                'min_pressure': -1000, 'max_pressure': 1000, 'P': 5, 'I': 0.00015,
-                'D': 0, 'bfs_instr_ID': None, 'comm_lock': ob1_comm_lock,
-                'calib_path': './resources/ob1_calib.txt'},
-                'ctrl_args': {}}
-            ]
 
         setup_valves = [
             {'name': 'Coflow Sheath', 'args': ['Cheminert', 'COM7'],
@@ -639,30 +627,30 @@ if __name__ == '__main__':
     fm_frame.Show()
 
 
-    if exp_type == 'coflow':
-        # For OB1 with feedback
-        fm_local_cmd_q = deque()
-        fm_local_ret_q = deque()
-        fm_local_status_q = deque()
+    # if exp_type == 'coflow':
+    #     # For OB1 with feedback
+    #     fm_local_cmd_q = deque()
+    #     fm_local_ret_q = deque()
+    #     fm_local_status_q = deque()
 
-        fm_comm_thread.add_new_communication('local', fm_local_cmd_q,
-            fm_local_ret_q, fm_local_status_q)
+    #     fm_comm_thread.add_new_communication('local', fm_local_cmd_q,
+    #         fm_local_ret_q, fm_local_status_q)
 
-        cmd = ['get_bfs_instr_id', [setup_fms[1]['name'],], {}]
+    #     cmd = ['get_bfs_instr_id', [setup_fms[1]['name'],], {}]
 
-        bfs_instr_id = utils.send_cmd(cmd, fm_local_cmd_q, fm_local_ret_q,
-            threading.Event(), threading.Lock(), False, 'fm', True)
+    #     bfs_instr_id = utils.send_cmd(cmd, fm_local_cmd_q, fm_local_ret_q,
+    #         threading.Event(), threading.Lock(), False, 'fm', True)
 
-        # cmd = ['start_remote', [setup_fms[1]['name'],], {}]
+    #     # cmd = ['start_remote', [setup_fms[1]['name'],], {}]
 
-        # utils.send_cmd(cmd, fm_local_cmd_q, fm_local_ret_q, threading.Event(),
-        #     threading.Lock(), False, 'fm', False)
+    #     # utils.send_cmd(cmd, fm_local_cmd_q, fm_local_ret_q, threading.Event(),
+    #     #     threading.Lock(), False, 'fm', False)
 
-        fm_comm_thread.remove_communication('local')
+    #     fm_comm_thread.remove_communication('local')
 
-        # setup_pumps[1]['kwargs']['bfs_instr_ID'] = bfs_instr_id
-        setup_pumps[1]['kwargs']['bfs_instr_ID'] = bfs_instr_id
-        setup_pumps[1]['kwargs']['fm_comm_lock'] = outlet_fm_comm_lock
+    #     # setup_pumps[1]['kwargs']['bfs_instr_ID'] = bfs_instr_id
+    #     setup_pumps[1]['kwargs']['bfs_instr_ID'] = bfs_instr_id
+    #     setup_pumps[1]['kwargs']['fm_comm_lock'] = outlet_fm_comm_lock
 
     pump_comm_thread = control_server_pump.get_comm_thread('pump')
 
