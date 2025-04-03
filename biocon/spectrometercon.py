05a5de523840b7c62dcfa8debceaa55ff09f625e
jbhopkins <hopkins.jesse@gmail.com>
2025-04-03 15:45:33 -0500

    Updates to base UV measurement on exposure period not exposure time. Needs testing.

--- biocon/spectrometercon.py
+++ biocon/spectrometercon.py
@@ -3624,17 +3624,17 @@ class UVPanel(utils.DevicePanel):
             num_frames = exp_params['num_frames']
             num_trig = int(exp_params['num_trig'])
 
-            if exp_time < 0.125 or exp_period - exp_time < 0.01:
+            if exp_period < 0.125 or exp_period - exp_time < 0.01:
                 uv_valid = False
 
             else:
                 max_int_t = float(self.int_time.GetValue())
 
-                int_time = min(exp_time/2, max_int_t)
+                int_time = min(exp_period/2, max_int_t)
 
                 spec_t = max(int_time*self.settings['int_t_scale'], 0.05)
 
-                scan_avgs = exp_time // spec_t
+                scan_avgs = exp_period // spec_t
 
                 abort_cmd = ['abort_collection', [self.name,], {}]
                 self._send_cmd(abort_cmd)
