5599ba7c71925f344eb03587767e002f029addb7
jbhopkins <hopkins.jesse@gmail.com>
2026-02-16 13:43:07 -0600

    Updated settings in biocon and expcon.

--- biocon/biocon.py
+++ biocon/biocon.py
@@ -538,17 +538,17 @@ if __name__ == '__main__':
 
     components = OrderedDict([
         ('exposure', expcon.ExpPanel),
-        # ('coflow', coflowcon.CoflowPanel),
+        ('coflow', coflowcon.CoflowPanel),
         # ('trsaxs_scan', trcon.TRScanPanel),
         # ('trsaxs_flow', trcon.TRFlowPanel),
         # ('scan',    scancon.ScanPanel),
-        # ('metadata', metadata.ParamPanel),
-        # ('pipeline', pipeline_ctrl.PipelineControl),
-        # ('uv', spectrometercon.UVPanel),
-        # ('hplc', biohplccon.HPLCPanel),
-        # ('automator', autocon.AutoPanel),
-        # ('autosampler', autosamplercon.AutosamplerPanel),
-        ('toaster', toastcon.ToasterPanel),
+        ('metadata', metadata.ParamPanel),
+        ('pipeline', pipeline_ctrl.PipelineControl),
+        ('uv', spectrometercon.UVPanel),
+        ('hplc', biohplccon.HPLCPanel),
+        ('automator', autocon.AutoPanel),
+        ('autosampler', autosamplercon.AutosamplerPanel),
+        # ('toaster', toastcon.ToasterPanel),
         ])
 
     settings = {
