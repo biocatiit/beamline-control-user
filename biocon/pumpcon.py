5a4bf6935f0426ecdc5e692ebb8cb46e650f5043
jbhopkins <hopkins.jesse@gmail.com>
2026-09-17 10:38:58 -0500

    Updated pump devices

--- biocon/pumpcon.py
+++ biocon/pumpcon.py
@@ -6481,22 +6481,22 @@ if __name__ == '__main__':
 
 
 
-    # # Coflow with OB1
-    bfs = fmcon.BFS('outlet_fm', 'COM5')
+    # # # Coflow with OB1
+    # bfs = fmcon.BFS('outlet_fm', 'COM5')
 
-    ob1_comm_lock = threading.RLock()
+    # ob1_comm_lock = threading.RLock()
 
-    setup_devices = [
-        # {'name': 'sheath', 'args': ['VICI M50', 'COM6'],
-        #     'kwargs': {'flow_cal': '627.72', 'backlash_cal': '9.814'},
-        #     'ctrl_args': {'flow_rate': 1}},
-        {'name': 'outlet', 'args': ['OB1 Pump', 'COM8'],
-            'kwargs': {'ob1_device_name': 'Outlet OB1', 'channel': 1,
-            'min_pressure': -1000, 'max_pressure': 1000, 'P': -2, 'I': -0.15,
-            'D': 0, 'bfs_instr_ID': bfs.instr_ID, 'comm_lock': ob1_comm_lock,
-            'calib_path': './resources/ob1_calib.txt'},
-            'ctrl_args': {}}
-        ]
+    # setup_devices = [
+    #     # {'name': 'sheath', 'args': ['VICI M50', 'COM6'],
+    #     #     'kwargs': {'flow_cal': '627.72', 'backlash_cal': '9.814'},
+    #     #     'ctrl_args': {'flow_rate': 1}},
+    #     {'name': 'outlet', 'args': ['OB1 Pump', 'COM8'],
+    #         'kwargs': {'ob1_device_name': 'Outlet OB1', 'channel': 1,
+    #         'min_pressure': -1000, 'max_pressure': 1000, 'P': -2, 'I': -0.15,
+    #         'D': 0, 'bfs_instr_ID': bfs.instr_ID, 'comm_lock': ob1_comm_lock,
+    #         'calib_path': './resources/ob1_calib.txt'},
+    #         'ctrl_args': {}}
+    #     ]
 
     #OB1 by itself
     # bfs = fmcon.BFS('outlet_fm', 'COM6')
